1e6e870c1ac4a4cbca79b3d3897d30fc355c23c1
Priscilla Finder <priscilla@Priscillas-MacBook-Pro.local>
2017-10-09 12:34:24 -0700

    finished create

--- app/controllers/dojos_controller.rb
+++ app/controllers/dojos_controller.rb
@@ -3,4 +3,20 @@ class DojosController < ApplicationController
   	@dojos = Dojo.all
   	render "dojos/index.html.erb"
   end
+  
+  def show_create_page
+  	render "dojos/create_page.html.erb"
+  end
+
+  def create_dojo
+  	dojo = Dojo.new(dojo_params)
+  	dojo.save
+  	redirect_to "/dojos"
+  end
+
+  private 
+  def dojo_params
+  	params.require(:dojo).permit(:branch, :street, :city, :state)
+  end
+
 end
